2952812124534c6d133e0463e7dd5eb7eac8f01c
lUkAC2 <113376248+lUkAC1234@users.noreply.github.com>
2023-10-08 17:08:21 +0500

    JAVASCRIPT LEARNING

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
-let text = document.querySelector('.text');
-let input = document.getElementById('input');
-let btn = document.getElementById('btn');
-let body = document.body
+/* TESTING TEXT STYLE CHANGERS */
+
+// let text = document.querySelector('.text');
+// let input = document.getElementById('input');
+// let btn = document.getElementById('btn');
+// let body = document.body
 
 
-btn.addEventListener('click', () => {
-    text.textContent = input.value + ' ' + 'changed'
+// btn.addEventListener('click', () => {
+//     text.textContent = input.value + ' ' + 'changed'
+//     text.style.color = 'red';
+    
+//     if (input.value === '') {
+//         text.textContent = '...';
+//         text.style.color = 'unset';
+//         setTimeout(() =>{
+//             body.style.background = 'red';
+//         }, 500) 
+//     }
+//     else {
+//         body.style.background = 'unset';
+//     }
+// })
 
-    text.style.color = 'red';
-})
+/* TESTING IMAGE CHANGER FUNCTION */
+
+// let input = document.getElementById('fileChanger');
+// let img = document.getElementById('img');
+
+// input.addEventListener('change', (event) => {
+//     img.src = URL.createObjectURL(event.target.files[0]);
+// });
+
+/* TESTING Calculation */
+
+// let inputFirst = document.getElementById('inputFirst')
+// let inputSecond = document.getElementById('inputSecond')
+// let btn = document.getElementById('btn')
+// let result = document.getElementById('result')
+
+// btn.addEventListener('click', () => {
+//     value1 =  parseFloat(inputFirst.value);
+//     value2 =  parseFloat(inputSecond.value);
+//     sum = value1 + value2;
+//     result.textContent = 'Result' + ': ' + sum;
+// })
